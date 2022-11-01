3d1a6f56f61fe074760adff5b7741bd908ee143f
thegeek13242 <bt20cse214@iiitn.ac.in>
2022-11-01 23:10:13 +0530

    ViewAllBookings Added

---
+++ client/src/components/ViewAllBookings/ViewAllBookings.js
@@ -0,0 +1,28 @@
+import "./ViewAllBookings.css";
+import { useSelector } from "react-redux";
+import { getAllBookings } from "../../api";
+
+const ViewAllBookings = () => {
+  const user = useSelector((state) => state.user);
+  const bookings = getAllBookings();
+  console.log(bookings);
+  
+  return (
+    <div className="view-all-bookings-container">
+      <h1 className="heading">All Bookings</h1>
+      <div className="bookings-container">
+        {bookings.map((booking) => {
+          return (
+            <div className="booking-container">
+              <h2 className="heading">Booking ID: {booking.bookingId}</h2>
+              <h3 className="heading">Train ID: {booking.trainDetails}</h3>
+              <h3 className="heading">User ID: {booking.userDetails}</h3>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ViewAllBookings;
